dd7a9ecb3c2b4c740d081343a70636e6b3bdba47
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 01:13:20 +0200

    create report Object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,5 +1,8 @@
 export default function createReportObject(employeesList) {
-  
+  const object = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      let nb = 0;
       for (const key in employeesList) if (employeesList[key]) nb += 1;
       return (nb);
     },
