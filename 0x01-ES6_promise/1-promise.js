bfec61fc8f93815dceb4683d0244d259b1ed71f2
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 01:31:49 +0200

    you promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,4 @@
-export default function getFullResponseFromAPI(success) {
-  if (success === true) {
-    return new Promise((resolve) => {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    });
-  }
+
   return new Promise((resolve, reject) => {
     reject(new Error('The fake API is not working currently'));
   });
