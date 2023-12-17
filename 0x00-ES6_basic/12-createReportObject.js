94ef81e55dca670d6cbf1d469d027722c3c655b5
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 01:12:38 +0200

    one a aprt ?

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  
+      for (const key in employeesList) if (employeesList[key]) nb += 1;
+      return (nb);
+    },
+  };
+
+  return object;
+}
