d9b78d326ce2f9aed1793938b832d8022ac698ca
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 01:31:05 +0200

    await 100th

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,13 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   let result = {};
 
-  try {
-    const user = await createUser();
-    const photo = await uploadPhoto();
-    result = { photo, user };
-  } catch (error) {
-    result = { photo: null, user: null };
-  }
+  
 
   return result;
 }
