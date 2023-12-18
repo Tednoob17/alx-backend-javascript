20ea36f4c3ff230d5f6c88b3a5974e46165143d2
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:21:26 +0200

    typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,9 +3,6 @@ export default function createInt8TypedArray(length, position, value) {
 
   const buffer = new DataView(
     new ArrayBuffer(length),
-    0,
-    length,
-  );
-  buffer.setInt8(position, value);
+  
   return buffer;
 }
