3f540d4a8ebab59b0b3943885421d56f3b58d8b2
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:20:34 +0200

    del eslink commecnt

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,6 @@
-/* eslint-disable no-param-reassign */
+
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      for (const g of newGrades) if (g.studentId === student.id) student.grade = g.grade;
-      if (student.grade === undefined) student.grade = 'N/A';
+  returnudent.grade = 'N/A';
       return student;
     });
 }
