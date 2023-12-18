5a0db84a9921cd261dee23280880e8c4aaf8e466
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 02:04:55 +0200

    make by trace

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -9,14 +9,4 @@ export default function initializeRooms() {
 
   return rooms;
 }
-import Classroom from './0-classroom';
 
-export default function initializeRooms () {
-    const rooms = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34),
-    ];
-
-    return rooms;
-}
