2280eb0e76b5fae50c96e815f72ceeb703dfc253
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:21:49 +0200

    SetInt 8 error

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,6 +3,9 @@ export default function createInt8TypedArray(length, position, value) {
 
   const buffer = new DataView(
     new ArrayBuffer(length),
-  
+    0,
+    length,
+  );
+  buffer.setInt8(position, value);
   return buffer;
 }
