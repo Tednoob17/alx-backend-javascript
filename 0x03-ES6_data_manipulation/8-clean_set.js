8fdd2701e07c828d36a25619af1fafb775fd5310
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:25:00 +0200

    Claen set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const values = [];
 
-  if (startString === '' || typeof startString !== 'string') return '';
+  if (startString === '' || typeof 
   set.forEach((s) => {
     if (typeof s === 'string' && s.startsWith(startString)) values.push(s.slice(startString.length));
   });
