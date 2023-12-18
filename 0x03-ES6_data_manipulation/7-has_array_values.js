c0f7ecceab03c1a5863ff6e889e32e138b04e62d
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:23:36 +0200

    has array value

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,6 @@
 export default function hasValuesFromArray(set, array) {
-  
+  const iterator = array.values();
+  for (const elem of iterator) if (set.has(elem) === false) return false;
   return true;
 }
+
