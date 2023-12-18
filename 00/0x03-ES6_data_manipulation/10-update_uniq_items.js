408169c3250a31d22769e8caa4814427f7e6a3ab
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:30:45 +0200

    uniq itemm

--- 00/0x03-ES6_data_manipulation/10-update_uniq_items.js
+++
@@ -1,6 +0,0 @@
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw new Error('Cannot process');
-  return map.forEach((key, val) => {
-    if (key === 1) map.set(val, 100);
-  });
-}
