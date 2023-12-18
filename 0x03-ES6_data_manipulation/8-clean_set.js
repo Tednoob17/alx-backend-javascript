5985be0ef77f62c7ca9d343d2b8f902eb154d403
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:25:11 +0200

    del all

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++
@@ -1,9 +0,0 @@
-export default function cleanSet(set, startString) {
-  const values = [];
-
-  if (startString === '' || typeof 
-  set.forEach((s) => {
-    if (typeof s === 'string' && s.startsWith(startString)) values.push(s.slice(startString.length));
-  });
-  return values.join('-');
-}
