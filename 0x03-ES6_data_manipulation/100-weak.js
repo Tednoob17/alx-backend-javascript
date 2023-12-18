36fd913e86abd044800115c2acbe926b28bb57fc
Ted Kouhouenou <teddams047@gmail.com>
2023-12-18 03:10:48 +0200

    broken chain

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,7 +3,5 @@ export const weakMap = new WeakMap();
 export function queryAPI(object) {
   if (weakMap.has(object)) {
     const data = weakMap.get(object);
-    if (data >= 4) throw new Error('Endpoint load is high');
-    weakMap.set(object, (data + 1));
-  } else weakMap.set(object, 1);
+   
 }
